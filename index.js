da6198aac6a2c2d9d2fd1a1f33f19641127c85a9
Luke Gill <bagrad007@gmail.com>
2021-04-12 13:48:55 -0400

    commit tree 3ce7548989cfdfd148250826f5676c69defd812c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(people, event) {
+    let arr = []
+    for (let i = 0; i < people.length; i++) {
+
+        arr.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return arr
+}
+
+function countDown(num) {
+    for (let i = num; i > -1; i--) {
+        console.log(i)
+    }
+}
